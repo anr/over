1ab9fbf151ef5b695db4c56212d9d77ae0e01744
Adriano Nagel <anr@binoklo.com>
2022-08-21 19:39:22 +0100

    Add start of a test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,27 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/chicago/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('foo', () => {
+  window.fetch = jest.fn(() => Promise.resolve({
+    ok: true,
+    json: () => Promise.resolve(
+      {
+        data: [
+          {
+            id: 123,
+            title: "Saxon Vase",
+            place_of_origin: "England",
+          },
+        ],
+      }
+    ),
+  })) as jest.Mock;
+
+  render(<App />);
+
+  expect(window.fetch).toBeCalled();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>Art Institute of Chicago</p>
         <Art />
       </header>
     </div>
